d69459e02f67cc0b0c8b07de5e49df71544d073a
Sayon-okai <sayon.study@gmail.com>
2025-10-14 20:21:38 +0000

    layout updated -- grid lay - no js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+let addIcon = '<i><img src="./assets/images/icon-plus.svg" alt="plus icon" class="icons-add icons"></i>';
+
+let minusIcon = '<i><img src="./assets/images/icon-minus.svg" alt="plus icon" class="icons-minus"></i>';
+
+// Adding addIcon to accordion
+$("h3").after(addIcon);
+
+    
+ 
