430621fd8e4e2bbbbec9c757a5622b9cd399babc
Sayon-okai <sayon.study@gmail.com>
2025-10-14 21:10:05 +0000

    code refactor - js logic implemented

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
 
-let addIcon = '<i><img src="./assets/images/icon-plus.svg" alt="plus icon" class="icons-add icons"></i>';
+let addIcon = '<img src="./assets/images/icon-plus.svg" alt="plus icon" class="icons-add icons">';
 
 let minusIcon = '<i><img src="./assets/images/icon-minus.svg" alt="plus icon" class="icons-minus"></i>';
 
-// Adding addIcon to accordion
-$("h3").after(addIcon);
 
-    
+  $(".icons").click(function () {
+      var faqAnswer = $(this).next("p");
+      faqAnswer.toggleClass("hidden")
+      
+
+      if (faqAnswer.hasClass("hidden")) {
+          $(this).attr("src", "./assets/images/icon-plus.svg");
+  
+          $(this).prev("h3").css("color", "")
+          
+         
+      } else {
+          $(this).attr("src", "./assets/images/icon-minus.svg");
+         
+          $(this).prev("h3").css("color", "hsla(292, 82%, 23%, 1.00)")
+              faqAnswer.addClass("fade-text");
+      }
+
+      
+   
+});
+
  
